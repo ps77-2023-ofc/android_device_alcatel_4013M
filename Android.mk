300bd87165cb5bed640d10171f4e36177f5ad558
Juan David Bogallo Quiroz <juandavidbogallo2020@gmail.com>
2024-01-23 16:10:59 -0500

    Android.mk: Add inclusion commands for Makefile files

--- Android.mk
+++ Android.mk
@@ -23,5 +23,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),4013M)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
